97b5d57d60968d7a3068818b4eb903f23b8a09d2
Julyemerson Leonizio <jb.leonizio@gmail.com>
2020-04-03 23:10:18 -0300

    não sei o que fiz de alteraçao para comitar

--- dist/js/mail.js
+++ dist/js/mail.js
@@ -1,35 +1,39 @@
 const express = require('express');
-const nodemailer = require('nodemailer')
+const nodemailer = require('nodemailer');
 const env = require('dotenv').config();
 const app = express();
 const port = 3000;
 
 const user = process.env.USER_EMAIL;
 const pass = process.env.USER_PASSWORD;
+const personSend = 'thalyta.apuhena@gmail.com';
 
+//app.get('/', (req, res) => res.send('Hello World'))
 
-app.get('/', (req, res) => res.send('Hello World'))
+//app.get('/send', (req, res) => {
+function sendEmail(from, to) {
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: { user, pass }
+  });
 
-app.get('/send', (req, res) => {
+  transporter.sendMail({
+    from: user,
+    to: personSend,
+    replyTo: 'jb.leonizio@gmail.com',
+    subject: 'Teste de envio de email via sistema ',
+    text: 'Recebemos sua mensagem e em breve entraremos em contato'
+  });
+  // .then(info => {
+  // res.send(info);
+  // })
+  // .catch(error => {
+  //  res.send(error);
+  //});
+}
+//});
 
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        auth: { user, pass }
-    });
+sendEmail(user, personSend);
 
-    transporter.sendMail({
-        from: user,
-        to: 'thalyta.apuhena@gmail.com',
-        replyTo: "stefanini.julyemerson@grupofleury.com.br",
-        subject: "Teste de envio de email via sistema ",
-        text: "Recebemos sua mensagem e em breve entraremos em contato"
-    }).then(info => {
-        res.send(info)
-    }).catch(error => {
-        res.send(error)
-    })
-
-})
-
-app.listen(port, () => console.log(`example listening on port ${port}`))
+app.listen(port, () => console.log(`example listening on port ${port}`));
